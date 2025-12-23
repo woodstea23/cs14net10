a6c89d3094cb6a1dfbfc785cbfb8c9758c486260
Rob Wood <32367644+woodstea23@users.noreply.github.com>
2025-12-23 18:47:53 +0000

    Working on Chapter 2

---
+++ Chapter02/Variables/Program.cs
@@ -0,0 +1,6 @@
+﻿object height = 1.88; // Storing a double in an object.
+object name = "Amir"; // Storing a string in an object.
+Console.WriteLine($"{name} is {height} metres tall.");
+// int length1 = name.Length; // This gives a compile error!
+int length2 = ((string)name).Length; // Cast name to a string.
+Console.WriteLine($"{name} has {length2} characters.");
